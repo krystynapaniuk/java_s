fe4e8e4d2eb09633b2bb80e7f0afcfa19a1548f6
Krystyna Paniuk <derba.krystyna@gmail.com>
2023-10-08 14:15:04 +0300

    HW2

---
+++ js-homework2.js
@@ -0,0 +1,37 @@
+var a = '10km'
+var b = 10
+switch (a) {
+    case a:
+        text = '10000m';
+        console.log('10000m')
+        break
+    case b:
+        text = 10000;
+        console.log(10000)
+}
+
+
+var a = '1h'
+var b = 1
+switch (a) {
+    case a:
+        text = '60m';
+        console.log('60m')
+        break
+    case b:
+        text = 3600;
+        console.log (3600)
+
+
+}
+var a = '1kg'
+var b = 1
+switch (a) {
+    case a:
+        text = '1000g';
+        console.log('1000g')
+        break
+    case b:
+        text = 1000;
+        console.log (1000)
+}
