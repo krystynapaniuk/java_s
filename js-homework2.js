21c2b14ed8b44e5e82aeb193ed653661f9c79e97
Krystyna Paniuk <derba.krystyna@gmail.com>
2023-10-08 18:59:28 +0300

    orrect switch logic

--- js-homework2.js
+++ js-homework2.js
@@ -1,37 +1,16 @@
 var a = '10km'
-var b = 10
-switch (a) {
-    case a:
-        text = '10000m';
+switch (a)
+{
+    case '10km':
         console.log('10000m')
-        break
-    case b:
-        text = 10000;
-        console.log(10000)
-}
-
-
-var a = '1h'
-var b = 1
-switch (a) {
-    case a:
-        text = '60m';
+        break;
+    case '1h':
         console.log('60m')
-        break
-    case b:
-        text = 3600;
-        console.log (3600)
-
-
-}
-var a = '1kg'
-var b = 1
-switch (a) {
-    case a:
-        text = '1000g';
+        break;
+    case '1kg':
         console.log('1000g')
-        break
-    case b:
-        text = 1000;
-        console.log (1000)
+        break;
+    default:
+        console.log('error')
 }
+
