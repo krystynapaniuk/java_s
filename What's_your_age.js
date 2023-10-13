66d7552b0e01690e2b1d9afc1bac4585fb254982
Krystyna Paniuk <derba.krystyna@gmail.com>
2023-10-13 10:43:59 +0300

    Age_update

--- What's_your_age.js
+++ What's_your_age.js
@@ -1,20 +1,26 @@
 let yourAge = prompt("Вкажіть свій вік");
 console.log(yourAge);
-
-let countAge = "";
-let result;
-
-if (countAge >= 10 && countAge <= 20) {
-    result = "років" }
-
-else {
-    countAge = yourAge % 10
-    if (countAge === 1) {
-        result = "рік"
-    } else if (countAge >= 2 && countAge <= 4) {
-        result = "роки"
+if (yourAge <= 0) {
+    console.log("Вас не існує")
+}
+    else if (yourAge === false) {
+    console.log("Ви натиснули кнопку Cancel")
+}
+    else if (isNaN(yourAge)) {
+    console.log("Будь ласка, введіть число")
+} else {
+    let CountAgeTwoNumbers = yourAge % 100;
+    if (CountAgeTwoNumbers >= 11 && CountAgeTwoNumbers <= 14) {
+        console.log("Тобі " + yourAge + " років");
     } else {
-        result = "років"
+        let CountAgeOneNumber = CountAgeTwoNumbers % 10;
+        if (CountAgeOneNumber === 1) {
+            console.log("Тобі " + yourAge + " рік")
+        } else if (CountAgeOneNumber >= 2 && CountAgeOneNumber <= 4) {
+            console.log("Тобі " + yourAge + " роки")
+        } else if (CountAgeOneNumber === 0 || CountAgeOneNumber >= 5 && CountAgeOneNumber <= 9) {
+            console.log("Тобі " + yourAge + " років");
+        }
+
     }
 }
-console.log(yourAge + result);
